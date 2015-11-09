c8fa286003b717f54a5a8a5090c89738710d5294
ideyu <gy4ok4ok@gmail.com>
2015-11-10 01:10:03 +0200

    update index view

--- app.rb
+++ app.rb
@@ -15,5 +15,6 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@barbers = Barber.all 
+	erb :index
 end
